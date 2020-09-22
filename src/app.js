c4e9baa87bc86ba2be6a04c65b859dbee8a5c4c0
Dmytro Myronenko <dim@uain.su>
2020-09-22 14:45:20 +0300

    updated base template

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
+import {isValid} from "./utils";
 import './styles.css'
 
 const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value);
+})
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+
+    submitBtn.disabled = true;
+  //  async
+    console.log('Question', question)
+    input.value = '';
+    input.className = '';
+    submitBtn.disabled = false;
+
+
+  }
 
 
 
+}
